fa81d06841c2482157d2697eee82ce6d3b945760
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2019-03-26 20:57:10 +0100

    Header DONE!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor(props){ //Comes with props as parameter,so call super
+        super(props); //; adding/omitting semicolons ; is optional in JS, lets keep doing it for now
+        console.log('boo');
+
+        this.state = {
+            rows: [
+                <p key={1}>This is state row 2 </p>,
+                <p key={2}>This is state row 4 </p>,
+                <p key={3}>This is state row 6</p>
+            ]
+
+        }
+    }
+
   render() {
     return (
       <div className="App">
@@ -27,6 +41,7 @@ class App extends Component {
                  id="searchBarInput"
                  placeholder="Search for a movie or TV show..." />
           </div>
+          {this.state.rows}
       </div>
     );
   }
